55376b0d4f31dc65e2634c475d6be2917e623440
shanukafer98 <shanukafer98@gmail.com>
2024-07-15 15:56:29 +0530

    chore: Update Vercel configuration for deployment and proxy API to the new target URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
+        target: 'https://wedding-plannerback-ge2a313u1-shanukafer98s-projects.vercel.app',
+        secure: false, // Disable SSL certificate validation
+        changeOrigin: true,
       },
     },
   },
-
   plugins: [react()],
 });
