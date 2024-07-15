a1d163ffd63bff502bb3dd814d0eb214f94ba0b9
shanukafer98 <shanukafer98@gmail.com>
2024-07-15 09:20:32 +0530

    chore: Update Vercel configuration for deployment

--- api/index.js
+++ api/index.js
@@ -25,9 +25,12 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
+
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
