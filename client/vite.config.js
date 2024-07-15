6d9e7f8bcf6596aac788866f0f104208d4bb3fb9
shanukafer98 <shanukafer98@gmail.com>
2024-07-15 17:03:38 +0530

    chore: Update Vercel configuration to enable SSL certificate validation for API proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://wedding-plannerback-shanukafer98s-projects.vercel.app',
-        secure: false, // Disable SSL certificate validation
+        secure: true, // Disable SSL certificate validation
         changeOrigin: true,
       },
     },
